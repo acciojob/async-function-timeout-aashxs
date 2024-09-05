444afb2a251ec3dca4bd46921b0b2fd457bf37f6
Ashish <90548985+aashxs@users.noreply.github.com>
2024-09-05 08:51:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+let output=document.querySelector("#output")
+let text=document.querySelector("#text")
+let delay=document.querySelector("#delay")
+
+function display(){
+	const promise1=()=>{
+		return  new Promise((resolve,reject)=>{
+			setTimeout(()=>{
+				resolve(text.value)
+			},delay.value)
+		})
+	}
+
+	async function result(){
+		let ans=await promise1();
+		output.innerText=ans;
+	}
+
+	result();
+	
+}
